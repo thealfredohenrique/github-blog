b12b9c47d74a6489bd2c586f98e52887d7454586
Alfredo Henrique <thealfredohenrique@gmail.com>
2023-07-07 21:13:07 -0300

    feat(styles): Add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { ThemeProvider } from "styled-components";
+import GlobalStyle from "./styles/global";
+import defaultTheme from "./styles/themes/default";
+
 function App() {
-  return <></>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+    </ThemeProvider>
+  );
 }
 
 export default App;
