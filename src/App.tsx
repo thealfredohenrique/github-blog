2ded33c590134619fe8bbe96075875dbe7c1a6cb
Alfredo Henrique <thealfredohenrique@gmail.com>
2023-07-07 22:30:10 -0300

    feat(routes): Add route structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { ThemeProvider } from "styled-components";
+import { BrowserRouter } from "react-router-dom";
+import Router from "./Router";
 import GlobalStyle from "./styles/global";
 import defaultTheme from "./styles/themes/default";
-import Header from "./components/Header";
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Header />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+
       <GlobalStyle />
     </ThemeProvider>
   );
