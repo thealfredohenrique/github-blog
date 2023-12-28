2aeccbfe174f15831b7a480c54a05c75e1e1d1de
Alfredo Henrique <thealfredohenrique@gmail.com>
2023-12-28 17:37:23 -0300

    chore(ci): Add GitHub Actions

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { ThemeProvider } from "styled-components";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import Router from "./Router";
 import GlobalStyle from "./styles/global";
 import defaultTheme from "./styles/themes/default";
@@ -7,9 +7,9 @@ import defaultTheme from "./styles/themes/default";
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <BrowserRouter>
+      <HashRouter>
         <Router />
-      </BrowserRouter>
+      </HashRouter>
 
       <GlobalStyle />
     </ThemeProvider>
